cbfdd900f713278820712310175dd257f32366bb
Hertzy Tambwe <53703925+HertzyTambwe@users.noreply.github.com>
2020-10-03 18:14:29 +0200

    Add project files.

---
+++ RamdomProjert/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RamdomProjert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WapyNum wapyNum = new WapyNum();
+
+            Console.WriteLine(wapyNum.Nuber1);
+            
+        }
+    }
+}
---
+++ RamdomProjert/WapyNum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RamdomProjert
+{
+    public class WapyNum
+    {
+        private const string chaineNuber = "abcdefghijklmnopqrstuvwxyz1234567890";
+        private string chaineRandom;
+        public char Nuber1
+        {
+            get
+            {
+                return chaineRandom[random.Next(chaineRandom.Length)];
+            }
+        }
+        public char Nuber2
+        {
+            get
+            {
+                return chaineRandom[random.Next(chaineRandom.Length)];
+            }
+        }
+        public char Nuber3
+        {
+            get
+            {
+                return chaineRandom[random.Next(chaineRandom.Length)];
+            }
+        }
+        public char Nuber4
+        {
+            get
+            {
+                return chaineRandom[random.Next(chaineRandom.Length)];
+            }
+        }
+        private Random random;
+
+        public WapyNum()
+        {
+            Initialisateur();
+        }
+
+        private void Initialisateur()
+        {
+            random = new Random();
+            for (int i = 0; i < chaineNuber.Length; i++)
+            {
+                chaineRandom += chaineNuber[random.Next(chaineNuber.Length)];
+            }
+        }
+    }
+}
